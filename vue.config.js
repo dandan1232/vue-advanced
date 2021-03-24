92006f9ade9d762c04a162a2bfb8cfcb160c2799
dandan1232 <345652127@qq.com>
2021-03-25 04:18:18 +0800

    vue cli项目创建210324

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  //项目打包的输出目录，相对于当前项目路径
   outputDir: './dist',
+  //项目打包后的资源目录，相对于dist路径
   assetsDir: './assets/',
+  //打包后的入口页面index.html，要引用资源的时候，正确方式是./assets/css/style.css， 加上统一的./前缀
   publicPath: './',
+  //入口页面index.html. 相对于dist的路径
   indexPath: './index.html',
   devServer: {
     host: 'localhost', //主机
